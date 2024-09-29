358a42865eb9ef30d401132ce38138ebee10a6c1
Mickey <zmisa0053@gmail.com>
2024-09-29 18:20:07 +0500

    Dif PowerAlgorithms

---
+++ Lab1/AlgoritmWithSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public abstract class AlgorithmWithSteps
+    {
+        protected List<IterationData> iterationData;
+        protected int steps;
+
+        public AlgorithmWithSteps()
+        {
+            iterationData = new List<IterationData>();
+            steps = 0;
+        }
+
+        public abstract void MeasureIteration(int iterations);
+
+        public void Start(int iterations)
+        {
+            iterationData.Clear();
+            steps = 0;
+            MeasureIteration(iterations);
+        }
+
+        public List<IterationData> GetIterationData()
+        {
+            return iterationData;
+        }
+
+        public int GetSteps()
+        {
+            return steps;
+        }
+    }
+}
---
+++ Lab1/RecursivePower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class RecursivePower : AlgorithmWithSteps
+    {
+        public override void MeasureIteration(int exponent)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            PowerRecursive(2, exponent);
+
+            stopwatch.Stop();
+        }
+
+        private int PowerRecursive(int baseNumber, int exponent)
+        {
+            steps++;  
+            if (exponent == 0) return 1;
+
+            int result = PowerRecursive(baseNumber, exponent - 1);
+            iterationData.Add(new IterationData
+            {
+                IterationNumber = exponent,
+                TimeSpent = steps  
+            });
+
+            return baseNumber * result;
+        }
+    }
+}
